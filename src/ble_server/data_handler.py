83f7cd5f418f67558fe0e047b6bc370cbcca7403
jijinbei <hallucigenia2002@gmail.com>
2024-08-27 03:26:33 +0900

    get_ble_serverの移動

--- src/ble_server/data_handler.py
+++ src/ble_server/data_handler.py
@@ -1,6 +1,7 @@
+import requests
+import yaml
 import polars as pl
 from polars import DataFrame, Series
-import yaml
 
 
 class CompanyID:
@@ -11,6 +12,18 @@ class CompanyID:
     def get_Company(self, config: [int]) -> Series:
         # TODO
         return
+
+
+def get_ble_server(url: str) -> (int, pl.DataFrame):
+    j = requests.get(url).json()
+    ble_df = pl.DataFrame(j["ble"]).select(
+        pl.col("address").alias("MAC_ADDRESS"),
+        pl.col("rssi").alias("RSSI"),
+        pl.col("manufacture_id").cast(pl.List(pl.UInt8)).alias("Company_ID"),
+        pl.col("time").cast(pl.Datetime).alias("Time"),
+    )
+    device_id = j["device_id"]
+    return device_id, ble_df
 
 
 def filter_duplicates(df: DataFrame) -> DataFrame:
